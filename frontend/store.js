4825bee0cceec31749b7721a75a289b54046b162
Ferhan Syed <syedferhan10@gmail.com>
2022-04-15 13:42:43 -0500

    Configure store

--- frontend/store.js
+++ frontend/store.js
@@ -1,18 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import axios from 'axios';
-import { createLogger } from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunkMiddleware from 'redux-thunk';
-import rootReducer from './redux';
+import { configureStore } from "@reduxjs/toolkit";
+import preferencesReducer from './redux/preference';
 
-let middleware = [
-  thunkMiddleware.withExtraArgument({ axios }),
-  createLogger({ collapsed: true }
-)];
-
-export default createStore(
-  rootReducer,
-  composeWithDevTools(
-    applyMiddleware(...middleware)
-  )
-);
+export default configureStore({
+  reducer: {
+    preferences: preferencesReducer,
+  }
+});
